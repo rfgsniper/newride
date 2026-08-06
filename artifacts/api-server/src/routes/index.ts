75f3d20226386548c95bfc575059c7d1e8e5dd8e
rfgsniper <rfgsniper@gmail.com>
2026-08-06 21:18:00 +0100

    Update index.ts

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -1,8 +1,8 @@
 import { Router, type IRouter } from "express";
 import healthRouter from "./health";
+import listingsRouter from "./listings";
 
 const router: IRouter = Router();
-
 router.use(healthRouter);
-
+router.use(listingsRouter);
 export default router;
